a4988b7d0d8b95b4a82a45645f7655e9dd1683d3
sowndaryaashok22 <sowndarya.ashok22@gmail.com>
2021-09-04 21:43:00 -0700

    initial commit

---
+++ src/Job.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const Job = ({job}) => {
+    return (
+        <div>
+            <h1>{job}</h1>
+             
+     </div>
+
+     
+       
+    )
+}
+
+export default Job
